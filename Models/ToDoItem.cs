86ff49b71c8a8e4419a2f0f4894a36765a8b5c61
Bashimane Mamabolo <117764630+Bashimane-Mamabolo@users.noreply.github.com>
2025-07-10 16:43:28 +0200

    Added todoitem model

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskBell.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        //public string Description { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; } = false;
+        //public DateTime CreatedAt { get; set; } = DateTime.Now;
+        //public DateTime? CompletedAt { get; set; } = null;
+
+    }
+}
