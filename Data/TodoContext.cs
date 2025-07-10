eb189eab9354a0e0bcd050b1a466fb51012984d4
Bashimane Mamabolo <117764630+Bashimane-Mamabolo@users.noreply.github.com>
2025-07-10 16:55:36 +0200

    Configured in-memory dbcontext class

---
+++ Data/TodoContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskBell.Models;
+
+namespace TaskBell.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<TaskBell.Models.ToDoItem>().HasKey(t => t.Id);
+            //modelBuilder.Entity<TaskBell.Models.ToDoItem>().Property(t => t.Title).IsRequired().HasMaxLength(200);
+
+            modelBuilder.Entity<ToDoItem>(entity =>
+            {
+                entity.HasKey(t => t.Id);
+                entity.Property(t => t.Title).IsRequired().HasMaxLength(200);
+            });
+
+            // Additional configurations can be added here
+        }
+
+
+    }
+}
